e7a10b7fc90e5dec39d48c2d9b277b06c2f28527
Andrew Lee <alee4738@gmail.com>
2018-08-08 23:21:35 -0700

    Make O(n) solution based on pointers
    O(n) time, O(1) space.
    All test cases passed. 16.22th percentile

--- container-most-water.py
+++ container-most-water.py
@@ -1,32 +1,26 @@
+# based on leetcode solution: https://leetcode.com/problems/container-with-most-water/solution/#
+"""
+If we try to move the pointer at the longer line inwards, we won't gain any increase in area,
+since it is limited by the shorter line. But moving the shorter line's pointer could turn out
+to be beneficial, as per the same argument, despite the reduction in the width. This is done
+since a relatively longer line obtained by moving the shorter line's pointer might overcome
+the reduction in area caused by the width reduction.
+"""
+
 height = [1,8,6,2,5,4,8,3,7]
 
-# create points independent of position in list
-sorted_points = []
-for i in range(len(height)):
-    sorted_points.append((i, height[i]))
-sorted_points.sort(key=lambda x: x[1])
+# start at the ends (largest width)
+left, right = 0, len(height) - 1
 
-# maintain array of valid points left
-valid_points = [True for i in range(len(height))]
-leftmost_valid, rightmost_valid = 0, len(height) - 1
-
-# greedy part, starting from lowest point
 max_area = 0
-for p in sorted_points:
-    # match with farthest point
-    max_width = max(p[0] - leftmost_valid, rightmost_valid - p[0])
-
-    # height is guaranteed to be the lowest point's height
-    area = max_width * p[1]
-
+while right - left > 0:
+    area = (right - left) * min(height[left], height[right])
     max_area = max(max_area, area)
 
-    # invalidate lowest point
-    valid_points[p[0]] = False
-    # fix valid ends
-    while leftmost_valid < len(valid_points) and not valid_points[leftmost_valid]:
-        leftmost_valid = leftmost_valid + 1
-    while rightmost_valid > 0 and not valid_points[rightmost_valid]:
-        rightmost_valid = rightmost_valid - 1
+    # update based on possibly increasing area
+    if height[left] < height[right]:
+        left = left + 1
+    else:
+        right = right - 1
 
 print(max_area)
