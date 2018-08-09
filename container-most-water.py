986dbc2950a43c562d3b505ce8e2c1a4b9eff7de
Andrew Lee <alee4738@gmail.com>
2018-08-08 23:09:53 -0700

    Make greedy solution based on lowest point
    O(nlogn) time, O(n) space.
    Passes all test cases. 5.39th percentile

--- container-most-water.py
+++ container-most-water.py
@@ -1,18 +1,32 @@
-def area(point_1, point_2):
-    return abs(point_1[0] - point_2[0]) * min(point_1[1], point_2[1])
+height = [1,8,6,2,5,4,8,3,7]
 
+# create points independent of position in list
+sorted_points = []
+for i in range(len(height)):
+    sorted_points.append((i, height[i]))
+sorted_points.sort(key=lambda x: x[1])
 
-points = [1,8,6,2,5,4,8,3,7]
+# maintain array of valid points left
+valid_points = [True for i in range(len(height))]
+leftmost_valid, rightmost_valid = 0, len(height) - 1
 
-# Naive solution: for each pair, keep running max
+# greedy part, starting from lowest point
 max_area = 0
-max_p1, max_p2 = (1, 0), (2, 0)
-for i, a_i in zip(range(1, len(points) + 1), points):
-    for j, a_j in zip(range(1, len(points) + 1), points):
-        a = area((i, a_i), (j, a_j))
-        if a > max_area:
-            max_area = a
-            max_p1 = (i, a_i)
-            max_p2 = (j, a_j)
+for p in sorted_points:
+    # match with farthest point
+    max_width = max(p[0] - leftmost_valid, rightmost_valid - p[0])
+
+    # height is guaranteed to be the lowest point's height
+    area = max_width * p[1]
+
+    max_area = max(max_area, area)
+
+    # invalidate lowest point
+    valid_points[p[0]] = False
+    # fix valid ends
+    while leftmost_valid < len(valid_points) and not valid_points[leftmost_valid]:
+        leftmost_valid = leftmost_valid + 1
+    while rightmost_valid > 0 and not valid_points[rightmost_valid]:
+        rightmost_valid = rightmost_valid - 1
 
 print(max_area)
