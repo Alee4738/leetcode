d03000917f7f287d7282ff335d15c999c67d41cf
Andrew Lee <alee4738@gmail.com>
2018-08-29 23:42:33 -0700

    Optimize find_first_last_pos.py, save your spot
    O(log n) time, O(1) space.
    88 / 88 test cases passed. Runtime: 36 ms. 100th percentile.
    Use binary search until you found a matching target, save your spot,
    then split into 2 binary searches for leftmost and rightmost indices.
    Warning: lots of repeated code (binary search process)

--- find_first_last_pos.py
+++ find_first_last_pos.py
@@ -9,57 +9,72 @@ Your algorithm's runtime complexity must be in the order of O(log n).
 If the target is not found in the array, return [-1, -1].
 """
 
-
-def find_first_pos(nums, target):
-	if not nums:
-		return -1
-
-	if len(nums) == 1:
-		return 0 if (nums[0] == target) else -1
-
-	# binary search with a twist
-	beg, end = 0, len(nums)
-	while end > beg:
-		mid = (beg + end) // 2
-		if nums[mid] > target:  # look left
-			end = mid
-		elif nums[mid] < target:  # look right
-			beg = mid + 1
-		# at this point, nums[middle index] is target
-		# check if it's the leftmost instance of target
-		elif mid == 0 or nums[mid - 1] < target:
-			return mid
-		else: # look left
-			end = mid
-	return -1
-
-
-def find_last_pos(nums, target):
-	if not nums:
-		return -1
-
-	if len(nums) == 1:
-		return 0 if (nums[0] == target) else -1
-
-	# binary search with a twist
-	beg, end = 0, len(nums)
-	while end > beg:
-		mid = (beg + end) // 2
-		if nums[mid] > target:  # look left
-			end = mid
-		elif nums[mid] < target:  # look right
-			beg = mid + 1
-		# at this point, nums[middle index] is target
-		# check if it's the leftmost instance of target
-		elif (mid == len(nums) - 1) or nums[mid + 1] > target:
-			return mid
-		else: # look right
-			beg = mid + 1
-	return -1
-
-
 def find_first_last_pos(nums, target):
-	return [find_first_pos(nums, target), find_last_pos(nums, target)]
+    if not nums:
+        return [-1, -1]
+        
+    elif len(nums) == 1:
+        return [0, 0] if (nums[0] == target) else [-1, -1]
+    
+    # get leftmost index
+
+    # binary search with a twist    
+    beg, end = 0, len(nums)
+    mid_break = -1
+    while end > beg:
+        mid = (beg + end) // 2
+        if nums[mid] > target:  # look left
+            end = mid
+        elif nums[mid] < target:  # look right
+            beg = mid + 1
+        # at this point, nums[middle index] == target
+        # expand to find leftmost and rightmost
+        else:
+        	mid_break = mid
+        	beg_break = beg
+        	end_break = end
+        	break
+    # case: didn't find any instance of target
+    if mid_break == -1:
+        return [-1, -1]
+    
+    # binary search for leftmost
+    beg, end = beg_break, mid_break + 1
+    while end > beg:
+        mid = (beg + end) // 2
+        if nums[mid] > target:  # look left
+            end = mid
+        elif nums[mid] < target:  # look right
+            beg = mid + 1
+        # at this point, nums[middle index] is target
+        # check if it's the leftmost instance of target
+        elif mid == 0 or nums[mid - 1] < target:
+            leftmost = mid
+            break
+        else: # look left
+            end = mid
+    if end <= beg:
+    	leftmost = -1
+
+    # binary search for rightmost
+    beg, end = mid_break, end_break
+    while end > beg:
+        mid = (beg + end) // 2
+        if nums[mid] > target:  # look left
+            end = mid
+        elif nums[mid] < target:  # look right
+            beg = mid + 1
+        # at this point, nums[middle index] is target
+        # check if it's the leftmost instance of target
+        elif (mid == len(nums) - 1) or nums[mid + 1] > target:
+            rightmost = mid
+            break
+        else: # look right
+            beg = mid + 1
+    if end <= beg:
+        rightmost = -1
+    
+    return [leftmost, rightmost]
 
 
 def main():
