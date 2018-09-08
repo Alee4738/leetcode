250ea50774f145045861debf4e84ac0626b74631
Andrew Lee <alee4738@gmail.com>
2018-09-08 14:14:25 -0700

    Make permutations, recursive soln, head and tail
    O(n!) time, O(n!) space. n size of nums
    25 / 25 test cases passed. Runtime: 60 ms. 38.56th percentile.
    Recursive solution splitting head and tail, recurse on tail

--- permutations.py
+++ permutations.py
@@ -4,9 +4,29 @@ Given a collection of distinct integers, return all possible permutations.
 import itertools
 import unittest
 
+# # itertools solution
+# def permute(nums):
+#     return list(list(p) for p in itertools.permutations(nums))
 
+
+# my own solution
 def permute(nums):
-    return list(list(p) for p in itertools.permutations(nums))
+    if not nums:
+        return []
+    elif len(nums) == 1:
+        return [nums]
+    else:
+        # split into head, tail
+        # put head in every possible position of permute(tail)
+        head = nums[0]
+        tail = nums[1:]
+        answers = []
+        for tail_perm in permute(tail):
+            for i in range(len(tail) + 1):
+                next_perm = tail_perm.copy()
+                next_perm.insert(i, head)
+                answers.append(next_perm)
+        return answers
 
 
 class TestPermute(unittest.TestCase):
