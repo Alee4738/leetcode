1f24d0752e1d2410ab0ed99475dbc3696196a6a9
Andrew Lee <alee4738@gmail.com>
2018-10-05 14:36:21 -0700

    Make match_regex, DP soln
    O(ls*lp)? time, O(ls*lp) space. ls len(s), lp len(p)
    447 / 447 test cases passed. Runtime: 104 ms. 31.7th percentile.
    The only difference is that I memoized everything

--- match_regex.py
+++ match_regex.py
@@ -34,10 +34,11 @@ def match_regex(s, p):
     :type p: str
     :rtype: bool
     """
-    return is_match(s, p, 0, 0)
+    memo = {}
+    return is_match(s, p, 0, 0, memo)
 
 
-def is_match(s, p, s_start, p_start):
+def is_match(s, p, s_start, p_start, memo):
     """
     :type s: List[str]
     :type p: List[str]
@@ -46,13 +47,19 @@ def is_match(s, p, s_start, p_start):
     :rtype: bool
     """
     # base cases
+    # it was memoized already
+    if (s_start, p_start) in memo:
+        return memo[(s_start, p_start)]
+
     # s and p both empty
-    if s_start == len(s) and p_start == len(p):
-        return True
+    elif s_start == len(s) and p_start == len(p):
+        memo[(s_start, p_start)] = True
+        return memo[(s_start, p_start)]
 
     # s not empty and p empty (so nothing matches s's next chars)
     elif s_start != len(s) and p_start == len(p):
-        return False
+        memo[(s_start, p_start)] = False
+        return memo[(s_start, p_start)]
 
     # s empty and p not empty
     elif s_start == len(s) and p_start != len(p):
@@ -61,10 +68,13 @@ def is_match(s, p, s_start, p_start):
         if (len(p) - p_start) % 2 == 0:
             for i in range(p_start + 1, len(p), 2):
                 if p[i] != '*':
-                    return False
-            return True
+                    memo[(s_start, p_start)] = False
+                    return memo[(s_start, p_start)]
+            memo[(s_start, p_start)] = True
+            return memo[(s_start, p_start)]
         else:
-            return False
+            memo[(s_start, p_start)] = False
+            return memo[(s_start, p_start)]
 
     else:
         # Now, s not empty and p not empty.
@@ -75,9 +85,11 @@ def is_match(s, p, s_start, p_start):
         if p_start + 1 == len(p) or p[p_start + 1] != '*':
             # match one-to-one
             if s[s_start] == p[p_start] or p[p_start] == '.':
-                return is_match(s, p, s_start + 1, p_start + 1)
+                memo[(s_start, p_start)] = is_match(s, p, s_start + 1, p_start + 1, memo)
+                return memo[(s_start, p_start)]
             else:
-                return False
+                memo[(s_start, p_start)] = False
+                return memo[(s_start, p_start)]
 
         # p's second char is *
         # Use * to match from 0 to max_matches, the max number of chars that can match p's first char
@@ -87,17 +99,20 @@ def is_match(s, p, s_start, p_start):
 
         # try all possible number of matches
         for num_matched in range(0, max_matches + 1):
-            if is_match(s, p, s_start + num_matched, p_start + 2):
-                return True
-        return False
+            ans = is_match(s, p, s_start + num_matched, p_start + 2, memo)
+            if ans:
+                memo[(s_start, p_start)] = True
+                return memo[(s_start, p_start)]
+        memo[(s_start, p_start)] = False
+        return memo[(s_start, p_start)]
 
 
-# assert(not match_regex('aa', 'a'))
-# assert(match_regex('aa', 'a*'))
-# assert(match_regex('ab', '.*'))
-# assert(match_regex('aab', 'c*a*b'))
-# assert(not match_regex('mississippi', 'mis*is*p*.'))
-# assert(match_regex('a', 'ab*'))
+assert(not match_regex('aa', 'a'))
+assert(match_regex('aa', 'a*'))
+assert(match_regex('ab', '.*'))
+assert(match_regex('aab', 'c*a*b'))
+assert(not match_regex('mississippi', 'mis*is*p*.'))
+assert(match_regex('a', 'ab*'))
 assert(match_regex('bbbba', '.*a*a'))
 
 
