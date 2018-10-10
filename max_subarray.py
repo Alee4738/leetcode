5011c8b14cb18287dc4b53e7591d986639cace46
Andrew Lee <alee4738@gmail.com>
2018-10-09 17:09:29 -0700

    Add comments to copy of max_subarray

--- max_subarray.py
+++ max_subarray.py
@@ -23,7 +23,7 @@ def maxSubArray(nums):
     """
     res, cur = nums[0], nums[0]
     for n in nums[1:]:
-        cur = max(cur + n, n)
+        cur = max(cur + n, n)   # you can either continue the current subarray or abandon it
         res = max(cur, res)
     return res
 
