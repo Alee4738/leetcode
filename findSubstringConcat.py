392faf20df0a1c8f24db95a1676b8a9321aa1e77
Andrew Lee <alee4738@gmail.com>
2019-04-27 01:32:39 -0700

    Make findSubstringConcat
    Runtime: 624 ms, faster than 44.05% of Python3
    Memory Usage: 14 MB, less than 5.10% of Python3

--- findSubstringConcat.py
+++ findSubstringConcat.py
@@ -1,7 +1,30 @@
+"""
+You are given a string, s, and a list of words, words, that are all of the same length. Find all starting indices of substring(s) in s that is a concatenation of each word in words exactly once and without any intervening characters.
+
+Example 1:
+
+Input:
+  s = "barfoothefoobarman",
+  words = ["foo","bar"]
+Output: [0,9]
+Explanation: Substrings starting at index 0 and 9 are "barfoor" and "foobar" respectively.
+The output order does not matter, returning [9,0] is fine too.
+
+Example 2:
+
+Input:
+  s = "wordgoodgoodgoodbestword",
+  words = ["word","good","best","word"]
+Output: []
+"""
+
 import unittest
 from typing import List
 
 def findSubstring(s: str, words: List[str]) -> List[int]:
+	if not s or not words:
+		return []
+
 	# maintain word list as frequency dictionary
 	wordsFreq = {}
 	for word in words:
@@ -11,18 +34,27 @@ def findSubstring(s: str, words: List[str]) -> List[int]:
 	wordSize = len(words[0])
 	result = []
 
-	# don't worry about string immutability
-	for start, char in enumerate(s):
-		currWords = {}
+	for start in range(wordSize):
+		# starting with index start, split entire string into a word list,
+		# which has words of size wordSize
+		wordList = []
+		for i in range(start, len(s), wordSize):
+			if i+wordSize <= len(s):
+				wordList.append((s[i:(i+wordSize)], i))
+
+		# print(wordList)
+
 		
-		if start + numWords * wordSize <= len(s):
-			for j in range(start, start + numWords * wordSize, wordSize):
-				word = s[j:(j + wordSize)]
+		# consider each numWords-length sublist of the word list
+		for i in range(len(wordList) - numWords + 1):
+			currWords = {}
+			for word, index in wordList[i:(i+numWords)]:
 				currWords[word] = currWords.setdefault(word, 0) + 1
 
 			if currWords == wordsFreq:
-				result.append(start)
+				result.append(wordList[i][1])
 
+	# print()
 	return result
 
 
@@ -37,10 +69,26 @@ class TestFindSubstring(unittest.TestCase):
 			findSubstring('wordgoodgoodgoodbestword', ['word','good','best','word']),
 			[])
 
+	def test_leetcode_3(self):
+		self.assertCountEqual(
+			findSubstring("lingmindraboofooowingdingbarrwingmonkeypoundcake",
+				["fooo","barr","wing","ding","wing"]),
+			[13])
+
 	def test_duplicates(self):
 		self.assertCountEqual(
 			findSubstring('wordgoodbestwordwordhelloworld', ['word','good','best','word']),
 			[0, 4])
+
+	def test_start_shifted(self):
+		self.assertCountEqual(
+			findSubstring('abbcab', ['ab', 'bc']),
+			[0, 2])
+
+		self.assertCountEqual(
+			findSubstring('gabbcab', ['ab', 'bc']),
+			[1, 3])
+		
 
 if __name__ == '__main__':
 	unittest.main()
