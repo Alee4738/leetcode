35f76420bfb51f8e6ba5f6899f8061a73ec09315
Andrew Lee <alee4738@gmail.com>
2021-03-25 22:11:10 -0700

    hasAllCodes Runtime: 232 ms, faster than 80.00% of TypeScript online submissions for Check If a String Contains All Binary Codes of Size K. Memory Usage: 58.5 MB, less than 24.61% of TypeScript online submissions for Check If a String Contains All Binary Codes of Size K.

---
+++ src/hasAllCodes.ts
@@ -0,0 +1,33 @@
+import { TestCase } from './testCase';
+
+function hasAllCodes(s: string, k: number): boolean {
+  // record substrings in a Set, use sliding window
+  // And if the size not equal to 2^k, then false
+  const seen = new Set<string>();
+  const windowSize = k;
+  const numPossibleBinaryStrings = Math.pow(2, windowSize);
+  for (let start = 0; start < s.length - (windowSize - 1); start++) {
+    const binaryNumber = s.substring(start, start + windowSize);
+    seen.add(binaryNumber);
+    if (seen.size === numPossibleBinaryStrings) {
+      return true;
+    }
+  }
+  return false;
+}
+
+describe(hasAllCodes.name, () => {
+  const testCases: TestCase<[string, number], boolean>[] = [
+    new TestCase(['00110110', 2], true),
+    new TestCase(['0110', 1], true),
+    new TestCase(['0110', 2], false),
+    new TestCase(['0000000001011100', 4], false),
+  ];
+
+  testCases.forEach((testCase) => {
+    it(testCase.desc ?? 'None', () => {
+      const actualResult = hasAllCodes(...testCase.input);
+      expect(actualResult).toEqual(testCase.expectedOutput);
+    });
+  });
+});
