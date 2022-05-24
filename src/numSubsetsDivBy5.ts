c4f4fc480d8ea4318d38efe5343992a5813202c9
Andrew Lee <alee4738@gmail.com>
2022-05-24 03:24:34 -0700

    numSubsetsDivByX faster DP solution

--- src/numSubsetsDivBy5.ts
+++ src/numSubsetsDivBy5.ts
@@ -4,108 +4,69 @@
 
 import { runTests, TestCase } from './testHelpers';
 
-// Return [0, 1, 2, ... size - 1]
-function firstIndices(size: number): number[] {
-  const subset = [];
-  let curSize = 0;
-  while (curSize < size) {
-    subset[curSize] = curSize;
-    curSize++;
-  }
-  return subset;
-}
+// Observation 1: Since I'm only concerned with divisibility, 1 is the same as 6, 2 as 7, 3 as 8, etc.
+// so instead of considering [1,2,...2000], I can say I have a dictionary<remainder, frequency>
+// { 0: 400, 1: 400, ... 4: 400 } and I choose numbers from that to get my subset.
 
-function getMaxIndices(indexArrLength: number, arrLength: number): number[] {
-  const ret = [];
-  let maxCurIndex = arrLength - indexArrLength;
+// Could this be solved using dynamic programming?
+// Sol(n) = func(Sol(n-1, ...), Sol(n-2, ...))
 
-  while (maxCurIndex < arrLength) {
-    ret.push(maxCurIndex);
-    maxCurIndex++;
-  }
-  return ret;
-}
+// Imagine I'm going from [1..4] to [1..5]. The next number is 5
+// any subset x of subsets of size 0 from [1..4] can become x + [5], becoming size 1
+// any subset x of subsets of size 1 from [1..4] can become x + [5], becoming size 2
+// any subset x of subsets of size 2 from [1..4] can become x + [5], becoming size 3
+// any subset x of subsets of size 3 from [1..4] can become x + [5], becoming size 4
+// any subset x of subsets of size 4 from [1..4] can become x + [5], becoming size 5
+// So, the number of subsets div by 5 at least doubles.
+// Actually, it's 2x + 1 because with the inclusion of 5, no subset whose remainder != 0
+// can become one with remainder = 0.
 
-// @param size must be > 0
-// @return next set indices if exists, otherwise undefined
-function nextIndices(indices: number[], size: number): number[] | undefined {
-  if (size <= 0) {
-    console.error('size <= 0');
-    return undefined;
-  }
-  const maxIndices = getMaxIndices(indices.length, size);
-  const ret = Array.from(indices);
-  let i = indices.length - 1;
-  while (i >= 0) {
-    const ithIndex = ret[i];
-    if (ithIndex < maxIndices[i]) {
-      ret[i]++;
-      // reset following indices
-      for (let j = i + 1; j < indices.length; j++) {
-        ret[j] = ret[j - 1] + 1;
-      }
-      return ret;
+// What if the next number is not remainder 0?
+// Generally, we'll have to keep track of the number of subsets whose sum is remainder 0,1,2,3,4
+
+// Think about the number of subsets of [1,2,...n]
+// It's 2^n because you can choose 1 to be in or out (2 choices), then choose 2 in or out,
+// and 3, 4, etc. = 2*2*2...*2 = 2^n
+// So as we're increasing n by 1, the number of subsets doubles. That's a good way to make sure
+// we're counting every subset there is.
+// Only the remainder matters.
+
+function numSubsetsDivByX(n: number, mod: number): BigInt {
+  // Idea: Build up from [] to [1] to [1,2] to [1,2,...2000]
+  // Keep track of the number of subsets whose sum has remainder 0,1,2,3...mod.
+  // remCount[i] is the number of subsets whose sum has remainder i.
+  // At first, the empty set has remainder 0, so remCount = [1, 0, 0, 0, ... 0]
+  let remCount = new Array<bigint>(mod);
+  remCount.fill(BigInt(0));
+  remCount[0] = BigInt(1);
+  for (let choice = 1; choice <= n; choice++) {
+    const nextRemCount = Array.from(remCount);
+    for (let rem = 0; rem < nextRemCount.length; rem++) {
+      // Looking at the subsets whose remainder is rem, if we decide to include choice,
+      // then it'll have a new remainder remWithChoice. These new subsets are different
+      // from anything we've ever counted because they include the new choice, so they
+      // can be directly added to the appropriate bucket of remCount.
+      const remWithChoice = (rem + choice) % mod;
+      nextRemCount[remWithChoice] = nextRemCount[remWithChoice] + remCount[rem];
     }
-    i--;
+    remCount = nextRemCount;
   }
-  return undefined;
+
+  return remCount[0];
 }
-
-function numSubsetsDivByX(max: number, x: number): number {
-  let count = 0;
-  for (let size = 0; size <= max; size++) {
-    let indices: number[] | undefined = firstIndices(size);
-    while (indices !== undefined) {
-      const subset = indices.map((val) => val + 1);
-      const sum = subset.reduce((acc, curr) => acc + curr, 0);
-      // console.log(subset, 'sum', sum);
-
-      if (sum % x === 0) {
-        count++;
-      }
-      indices = nextIndices(indices, max);
-    }
-  }
-  return count;
-}
-
-describe(nextIndices.name, () => {
-  const testCases: TestCase<[number[], number], number[] | undefined>[] = [
-    new TestCase([[0, 1, 2], 4], [0, 1, 3], 'increment last index'),
-    new TestCase(
-      [[0, 1, 3], 4],
-      [0, 2, 3],
-      'last digit is maxed, increment 2nd to last digit'
-    ),
-    new TestCase(
-      [[0, 2, 3], 4],
-      [1, 2, 3],
-      'last 2 digits maxed, increment 3rd to last digit'
-    ),
-    new TestCase(
-      [[0, 3, 4], 5],
-      [1, 2, 3],
-      'after incrementing, reset later indices'
-    ),
-    new TestCase(
-      [[1, 2, 3], 4],
-      undefined,
-      'all digits maxed, return undefined'
-    ),
-  ];
-
-  runTests(testCases, (testCase) => {
-    const actualResult = nextIndices(...testCase.input);
-    expect(actualResult).toEqual(testCase.expectedOutput);
-  });
-});
 
 fdescribe(numSubsetsDivByX.name, () => {
-  const testCases: TestCase<[number, number], number>[] = [
-    new TestCase([3, 5], 2), // [1,2,3] -> [], [2,3]
-    new TestCase([5, 5], 8), // [1,2,3,4,5] -> [], [5], [1,4], [2,3], [1,4,5], [2,3,5], [1,2,3,4], [1,2,3,4,5]
+  const testCases: TestCase<[number, number], BigInt>[] = [
+    new TestCase([3, 5], BigInt(2)), // [1,2,3] -> [], [2,3]
+    new TestCase([5, 5], BigInt(8)), // [1,2,3,4,5] -> [], [5], [1,4], [2,3], [1,4,5], [2,3,5], [1,2,3,4], [1,2,3,4,5]
     // My solution is too time-inefficient for the 3b1b case.
-    // new TestCase([2000, 5], 8),
+    new TestCase(
+      [2000, 5],
+      BigInt(
+        '22962613905485090484656664023553639680446354041773904009552854736515325227847406277133189726330125398368919292779749255468942379217261106628518627123333063707825997829062456000137755829648008974285785398012697248956323092729277672789463405208093270794180999311632479761788925921124662329907232844394066536268833781796891701120475896961582811780186955300085800543341325166104401626447256258352253576663441319799079283625404355971680808431970636650308177886780418384110991556717934409897816293912852988275811422719154702569434391547265221166310540389294622648560061463880851178273858239474974548427800576'
+      ),
+      '3Blue1Brown problem'
+    ),
   ];
 
   runTests(testCases, (testCase) => {
